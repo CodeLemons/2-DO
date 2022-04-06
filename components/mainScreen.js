b2aaecd69adf63f6e5c4839e426f71c5c35d3a7d
melonchmp <asamalihamza@gmail.com>
2022-04-06 13:01:07 +0100

    Added modals for main screen to transition to add task screen

--- components/mainScreen.js
+++ components/mainScreen.js
@@ -1,19 +1,55 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Modal, TouchableOpacity } from 'react-native';
 import dayName from '../shared/dayName';
 import ordinalNumbers from '../shared/ordinalNumbers';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 
 export default function MainScreen() {
+
+    const [modalOpen, setModalOpen] = useState(false);
+
     return (
-        <View>
-            <Text style={styles.headLine}>2-DO's on</Text>
-            <Text style={styles.date}>{dayName()}, {ordinalNumbers()}</Text>
+        <View style={styles.container}>
+            <Modal visible={modalOpen} animationType='fade'>
+                <View style={styles.modalContent}>
+                    <Text>HELLO WORLD!</Text>
+                    <TouchableOpacity>
+                        <Icon
+                            style={styles.modalToggleOff}
+                            name='checkcircle'
+                            size={60}
+                            color="#20212C"
+                            iconStyle={{padding: 2}}
+                            onPress={() => setModalOpen(false)}
+                        />
+                    </TouchableOpacity>
+                </View>
+                
+            </Modal>
+            <View>
+                <Text style={styles.headLine}>2-DOs on</Text>
+                <Text style={styles.date}>{dayName()}, {ordinalNumbers()}</Text>
+            </View>
+            <TouchableOpacity>
+                <Icon
+                    style={styles.modalToggleOn}
+                    name='pluscircle'
+                    size={60}
+                    color="#20212C"
+                    iconStyle={{padding: 2}}
+                    onPress={() => setModalOpen(true)}
+                />
+            </TouchableOpacity>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 5,
+    },  
     headLine: {
         fontSize: 30,
         fontWeight: 'bold',
@@ -32,6 +68,22 @@ const styles = StyleSheet.create({
         color: 'white',
         textAlign: 'left',
         paddingLeft: 40,
+    },
+    modalContent: {
+        flex: 1, 
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#17181F'
+    },
+    modalToggleOn: {
+        marginTop: 360,
+        padding: 10,
+        alignSelf: 'center'
+    },
+    modalToggleOff: {
+        marginTop: 510,
+        padding: 10,
+        alignSelf: 'center'
     }
     // reactangle: {
     //     position: 'absolute',
